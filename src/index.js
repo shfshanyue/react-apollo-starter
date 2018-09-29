42e4f3671865c165883655c625bd6b07a16f4853
shfshanyue <xianger94@gmail.com>
2018-09-29 18:11:29 +0800

    Add graphql package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://localhost:5000/graphql"
+});
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+), document.getElementById('root'));
+
 registerServiceWorker();
