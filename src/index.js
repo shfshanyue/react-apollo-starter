04d5a918fb2c5755d36f861ae1e872af567b76d9
shfshanyue <xianger94@gmail.com>
2018-09-29 19:15:38 +0800

    Add graphql query

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: 'http://localhost:5000/graphql',
+  fetch (uri, options) {
+    const body = JSON.parse(options.body)
+    // 方便调试
+    return fetch(`${uri}?query=${body.operationName || ''}`, options) 
+  }
 });
 
 ReactDOM.render((
