a45e0d1d6471bd6da5e0ff12045526e58bdb7a67
shfshanyue <xianger94@gmail.com>
2018-09-30 10:37:12 +0800

    Add header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { PING } from '@query';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+
+import Login from './pages/Login'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        { this.props.ping }
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            <div className="container app-container">
+              <div className="title">React Apollo Starter Kit</div>
+              <div className="app-list">
+                <div className="app-item"><Link to="/">Home</Link></div>
+                <div className="app-item"><Link to="/login">Login</Link></div>
+              </div>
+            </div>
+          </header>
+
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
 
-export default graphql(PING, {
-  props ({ data }) {
-    return {
-      ping: data.ping,
-    } 
-  }
-})(App)
+export default App;
