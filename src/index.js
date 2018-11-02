854305137cbb7893c3b6ea25b5a1bf1a817695b7
shfshanyue <xianger94@gmail.com>
2018-11-02 19:26:29 +0800

    Add httpstatus 401

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import createHistory from "history/createBrowserHistory"
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const history = createHistory()
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
@@ -13,6 +16,18 @@ const client = new ApolloClient({
     const body = JSON.parse(options.body)
     // 方便调试
     return fetch(`${uri}?query=${body.operationName || ''}`, options) 
+  },
+  onError ({ response, graphQLErrors }) {
+    if (graphQLErrors) {
+      const { message, httpStatus } = graphQLErrors[0]
+      console.log(httpStatus)
+      if (httpStatus === 401) {
+        localStorage.clear('token')
+        history.push('/')
+      } else {
+        console.log(message)
+      }
+    }
   },
   request (operation) {
     operation.setContext({
